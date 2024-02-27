098386f08594957ed6f6229285e83d01d52e6a1f
Maciej <M.Kozik@student.tudelft.nl>
2024-02-27 13:24:56 +0100

    Used the getComponent method of Vector where aplicable

--- Common/Matrix.cpp
+++ Common/Matrix.cpp
@@ -15,7 +15,7 @@ Vector Matrix::operator*(Vector v) {
     for (int i = 0; i < m; i++) temp.push_back(0.0);
     Vector res(temp);
     for (int i = 0; i < n; i++) {
-        res = res + columns.at(i) * v.getComponents().at(i);
+        res = res + columns.at(i) * v.getComponent(i);
     }
     return res;
 }
@@ -29,7 +29,7 @@ Vector Matrix::toVector() {
     vector<double> comp;
     comp.reserve(columns.size());
     for (Vector c: columns) {
-        comp.push_back(c.getComponents().at(0));
+        comp.push_back(c.getComponent(0));
     }
     return Vector(comp);
 }
@@ -39,7 +39,7 @@ string Matrix::toString(int precision) {
     if(m == 1){
         result += "( ";
         for(int i = 0; i < n; i++){
-            result += UsefulMethods::doubleToString(columns.at(i).getComponents().at(0),precision) + " ";
+            result += UsefulMethods::doubleToString(columns.at(i).getComponent(0),precision) + " ";
         }
         result += ")";
     }
@@ -49,7 +49,7 @@ string Matrix::toString(int precision) {
             else if(i ==m-1) result +="\\ ";
             else result +="| ";
             for(int j = 0; j < n; j++){
-                result += UsefulMethods::doubleToString(columns.at(j).getComponents().at(i),precision) + " ";
+                result += UsefulMethods::doubleToString(columns.at(j).getComponent(i),precision) + " ";
             }
             if(i == 0) result += "\\\n";
             else if(i ==m-1) result +="/\n";
--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@
 using namespace std;
 
 int main() {
-//    NeuralNetwork XOR({Vector({0,-1}),Vector({0,0})},{Matrix({Vector({1, 1}), Vector({1,1})}),Matrix({Vector({1}), Vector({-2})})});
-//    cout << XOR.output(Vector({0,0})).getComponents().at(0) << endl;
-//    cout << XOR.output(Vector({1,0})).getComponents().at(0) << endl;
-//    cout << XOR.output(Vector({0,1})).getComponents().at(0) << endl;
-//    cout << XOR.output(Vector({1,1})).getComponents().at(0) << endl;
+    NeuralNetwork XOR({Vector({0,-1}),Vector({0,0})},{Matrix({Vector({1, 1}), Vector({1,1})}),Matrix({Vector({1}), Vector({-2})})});
+    cout << XOR.output(Vector({0,0})).getComponent(0) << endl;
+    cout << XOR.output(Vector({1,0})).getComponent(0) << endl;
+    cout << XOR.output(Vector({0,1})).getComponent(0) << endl;
+    cout << XOR.output(Vector({1,1})).getComponent(0) << endl;
 
     Matrix m({Vector({1,2,3}), Vector({2,1,3}),Vector({4,1,7})});
-    cout << m.toString(0);
+    cout << endl << m.toString(0);
     return 0;
 }
